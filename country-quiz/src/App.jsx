253d0b7ed6103cf38ccb19c0891e73bad30a7827
Ted Dino <ted23@tutanota.com>
2022-07-04 18:36:52 +0800

    add generating random question

--- country-quiz/src/App.jsx
+++ country-quiz/src/App.jsx
@@ -1,7 +1,36 @@
+import { useState, useEffect } from "react";
+
 import "./App.css";
 import Question from "./components/Question";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState([]);
+  const [randomQuestion, setRandomQuestion] = useState("");
+
+  const fetchData = async () => {
+    try {
+      if (countries.length === 0) {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        const data = await response.json();
+        setCountries(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getRandomQuestion = () => {
+    const randomCountry =
+      countries[Math.floor(Math.random() * countries.length)];
+    setCountry(randomCountry);
+  };
+
+  useEffect(() => {
+    fetchData();
+    getRandomQuestion();
+  }, [countries]);
+
   return (
     <div className="App">
       <h1>Country quiz</h1>
