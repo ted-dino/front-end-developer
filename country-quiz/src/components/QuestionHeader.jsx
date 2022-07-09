994a90ce57431aaebc5ac79da8ead703893931f2
Ted Dino <ted23@tutanota.com>
2022-07-09 13:43:01 +0800

    add question header component

---
+++ country-quiz/src/components/QuestionHeader.jsx
@@ -0,0 +1,18 @@
+const QuestionHeader = ({ question, flag }) => {
+  return (
+    <div className="question__title">
+      {question === "Which country does this flag belong to?" ? (
+        <>
+          <img src={flag} alt="/" />
+          <h2>{question}</h2>
+        </>
+      ) : (
+        <>
+          <h2>{question}</h2>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default QuestionHeader;
