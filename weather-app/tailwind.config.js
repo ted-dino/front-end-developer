ec809ba2d2f98e60b8376bf86eae70b94515facc
Ted Dino <ted23@tutanota.com>
2022-07-12 15:20:13 +0800

    add custom colors in tailwind

--- weather-app/tailwind.config.js
+++ weather-app/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-primary": "#100e1d",
+        "color-secondary": "#1e213a",
+        "text-primary": "#e7e7eb",
+        "text-secondary": "#a09fb1",
+        "text-accent": "#88869d",
+        "btn-primary": "#6e707a",
+        "btn-secondary": "#585676",
+        "btn-accent": "#3c47e9",
+      },
+    },
   },
   plugins: [],
 };
