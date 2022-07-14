84a8c7134d51d083116b82e4cdb3f5fef36ffaeb
Ted Dino <ted23@tutanota.com>
2022-07-14 17:45:40 +0800

    add loading state

--- weather-app/src/hooks/weatherHook.js
+++ weather-app/src/hooks/weatherHook.js
@@ -4,13 +4,16 @@ const API_KEY = "CSKW65FQSTGUR8YJC5JMM7MMA";
 
 const useStore = create((set) => ({
   locationWeather: [],
+  isLoading: false,
   unitGroup: "",
   searchList: [],
   fetchData: async (city, unit) => {
+    set((state) => ({ isLoading: (state.isLoading = true) }));
     const response = await fetch(
       `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=${unit}&include=current%2Cdays&key=${API_KEY}&contentType=json`
     );
     set({ locationWeather: await response.json() });
+    set((state) => ({ isLoading: (state.isLoading = false) }));
   },
   addToSearchList: (query) => {
     set((state) => ({
