d72d9b7b30d885ff1cbf9e80a658300886e7a5c7
Ted Dino <ted23@tutanota.com>
2022-07-14 15:53:06 +0800

    import dynamic image from folder

---
+++ weather-app/src/hooks/iconHook.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+const useImage = (fileName) => {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      try {
+        const response = await import(
+          `../assets/weather-icons/${fileName}.png`
+        );
+        setImage(response.default);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchImage();
+  }, [fileName]);
+
+  return {
+    image,
+  };
+};
+
+export default useImage;
