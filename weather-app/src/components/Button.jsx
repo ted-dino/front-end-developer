44ef9c5e65202fdf341bde13a33c043d02eb6fe0
Ted Dino <ted23@tutanota.com>
2022-07-15 12:04:24 +0800

    Add reusable button component

---
+++ weather-app/src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({ className, onClick, children }) => {
+  return (
+    <button onClick={onClick} className={className}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
