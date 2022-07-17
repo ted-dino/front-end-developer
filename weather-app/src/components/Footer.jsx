7a751070211afab91b9ccf50fd08c81506c2f4a0
Ted Dino <ted23@tutanota.com>
2022-07-17 16:52:19 +0800

    add footer

---
+++ weather-app/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="text-center mt-16 mb-2">
+      created by &nbsp;
+      <a className="underline font-bold" href="https://github.com/ted-dino">
+        Ted Dino
+      </a>
+      &nbsp; - devchallenges.io
+    </footer>
+  );
+};
+
+export default Footer;
