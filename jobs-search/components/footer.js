7151cca5c35074f35c40cf8022e70577e11eba04
Ted Dino <ted23@tutanota.com>
2022-07-24 17:19:28 +0800

    add footer

---
+++ jobs-search/components/footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="text-center text-common">
+      created by &nbsp;
+      <a className="underline font-bold" href="https://github.com/ted-dino">
+        Ted Dino
+      </a>
+      &nbsp; - devchallenges.io
+    </footer>
+  );
+};
+
+export default Footer;
