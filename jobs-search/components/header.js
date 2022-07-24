24926013f787f5c9f890ab90fc1c32f77f85359f
Ted Dino <ted23@tutanota.com>
2022-07-24 17:19:40 +0800

    add header

---
+++ jobs-search/components/header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <h1 className="text-2xl flex gap-1 font-poppins">
+        <span className="text-[#282538] font-bold">Github</span>
+        <span className="font-light">Jobs</span>
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
