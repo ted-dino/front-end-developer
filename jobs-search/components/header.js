1c082a7aa126e367874733d47c9be96e2af8066c
Ted Dino <ted23@tutanota.com>
2022-07-26 13:57:47 +0800

    add link back to home

--- jobs-search/components/header.js
+++ jobs-search/components/header.js
@@ -1,10 +1,13 @@
+import Link from "next/link";
 const Header = () => {
   return (
     <header>
-      <h1 className="text-2xl flex gap-1 font-poppins">
-        <span className="text-[#282538] font-bold">Github</span>
-        <span className="font-light">Jobs</span>
-      </h1>
+      <Link href="/">
+        <h1 className="text-2xl flex gap-1 font-poppins cursor-pointer">
+          <span className="text-[#282538] font-bold">Github</span>
+          <span className="font-light">Jobs</span>
+        </h1>
+      </Link>
     </header>
   );
 };
