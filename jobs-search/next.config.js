c91079b9a42a880a8d20225a0fed0d263150a8af
Ted Dino <ted23@tutanota.com>
2022-07-27 15:07:21 +0800

    change image placholder domain

--- jobs-search/next.config.js
+++ jobs-search/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["remotive.com", "via.placeholder.com"],
+    domains: ["remotive.com", "fakeimg.pl"],
   },
 };
 
